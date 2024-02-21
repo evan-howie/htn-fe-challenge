5d440556172fb12067d73cef4cdc9b87722892e1
Evan Howie <etshowie@gmail.com>
2024-02-20 21:22:21 -0500

    changed login credentials

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 const dummyCredentials = {
-  email: "etshowie@gmail.com",
+  email: "abc@def.com",
   password: "bob",
 };
 
